063ebf856bfd2b04f3d5adbae3e3e6e4e8a64ebe
zlm <superming1991@163.com>
2020-11-16 11:40:14 +0800

    init

---
+++ RobotTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RobotTest
+//
+//  Created by 赵露明 on 2020/11/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, Robot!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RobotTest/RobotTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  RobotTestApp.swift
+//  RobotTest
+//
+//  Created by 赵露明 on 2020/11/16.
+//
+
+import SwiftUI
+
+@main
+struct RobotTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
